3a195d2d0f8f6054a207a2acd32b1f3e2b306388
kermage <genealyson.torcende@gmail.com>
2018-10-22 22:06:05 +0800

    update license

--- wiz-ee-wig-editor.php
+++ wiz-ee-wig-editor.php
@@ -12,8 +12,8 @@
  * Author URI:  mailto:genealyson.torcende@gmail.com
  * Description: A WYSIWYG widget using the wp_editor().
  * Version:     1.0.0
- * License:     GNU General Public License v2 or later
- * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * License:     GNU General Public License v3.0
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain: wewe
  */
 
